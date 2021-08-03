f39e2b1144f98efe1fc7e8756770666a4dacf3df
unknown <carloseverduzco@hotmail.com>
2021-08-02 22:41:50 -0600

    Console log delete

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,7 +1,6 @@
 import axios from 'axios';
 
 const apiKey = 'AIzaSyCl5EvhMOeOTn6K4SgBE2lzNeUsTUtZgfw';
-console.log(apiKey);
 export const youtubeClient = axios.create({
   baseURL: 'https://www.googleapis.com/youtube/v3/',
   params: {
